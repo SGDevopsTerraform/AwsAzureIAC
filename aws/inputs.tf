153e043af31d72728cfbdb66b09ac9f097e34ff2
Sandeep Gautam <sandeep.gutm@gmail.com>
2023-03-21 10:45:16 -0400

    Updated Terraform Providers and Resources for AWS TRG 1

--- aws/inputs.tf
+++ aws/inputs.tf
@@ -6,13 +6,13 @@ variable "sgapp-region" {
 
 variable "sgapp-vpc-range" {
   type        = string
-  default     = "192.168.0.0/16"
+  default     = "10.0.0.0/16"
   description = "VPC cidr range"
 }
 
 variable "sgapp-subnet-cidrs" {
   type    = list(string)
-  default = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+  default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.16.0/20", "10.0.32.0/20"]
   description = "VPC subnet cidr"
 }
 
@@ -24,6 +24,6 @@ variable "sgapp-subnet-azs" {
 
 variable "sgapp-subnet-names" {
   type    = list(string)
-  default = ["sg-app-private-subnet1", "sg-app-private-subnet2", "sgapp-db-subnet1", "sgapp-db-subnet2"]
+  default = ["sg-app-public-subnet1", "sg-app-public-subnet2", "sg-app-private-db-subnet1", "sg-app-private-db-subnet2"]
   description = "VPC subnet name"
 }
